3d4dff138599f9939ec9bd87beec57cd2c28a5d5
holyyatra <guptaasish842@gmail.com>
2025-04-03 04:51:42 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,23 +1,31 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  distDir: 'out',
-  eslint: {
-    // we use biome for linting
-    ignoreDuringBuilds: true,
-  },
+
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /**
+   * Enable static exports.
+   *
+   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+   */
+  output: "export",
+
+  /**
+   * Set base path. This is the slug of your GitHub repository.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+   */
+  basePath: "/nextjs-github-pages",
+
+  /**
+   * Disable server-based image optimization. Next.js does not support
+   * dynamic features with static exports.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
+   */
   images: {
     unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'ext.same-assets.com',
-      },
-    ],
-  },
-  typescript: {
-    ignoreBuildErrors: true,
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
